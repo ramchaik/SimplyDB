a78dbad641fdaeb51ff0a026263b8659ddfcb544
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-02-19 10:20:54 -0400

    feat: add constants

---
+++ src/main/java/dev/vaibhavsingh/constants/Constants.java
@@ -0,0 +1,7 @@
+package dev.vaibhavsingh.constants;
+
+public class Constants {
+    public static final String WELCOME_MESSAGE = "==================================================\n" +
+            "Hello there! 👋 Welcome to SimplyDB! 🎉\n" +
+            "==================================================";
+}
